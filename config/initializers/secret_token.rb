d88adeabf044828bf999c4267186a917dc73a94f
Aleksey verbivskyy <a.verbivskyy@svitla.com>
2013-05-24 08:37:27 +0300

    Add ru language and some small fixes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hytir::Application.config.secret_token = 'f1d9a33b86969f04b574c1c8f91ea839158c63340a927cbdbd332419bed4f1f0bd12184345d97f2245f36fef5ad9dcb0383bb251f0d4ff8f7973fdb6f054bf24'
